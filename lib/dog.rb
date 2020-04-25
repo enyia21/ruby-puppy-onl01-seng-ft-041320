76995043c08065d0a286a5c75e4d098708e50844
Chibuisi Enyia <enyia21@gmail.com>
2020-04-25 13:46:34 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Add your code here
+class Dog
+  @@all = []
+
+
+  def initialize(name)
+  @name = name
+  save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each {|dog| puts "#{dog}"}
+  end
+
+  def self.save
+    @@all << self
+  end
+end
